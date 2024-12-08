229cbb0d338755e432622f20f819ca6263c2a5e8
DerFrZocker <git@derfrzocker.de>
2024-12-08 06:41:49 +0100

    Advent Of Code 2024 Day 08 Part 2

---
+++ 2024/day-08/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,103 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File UPDATES = new File("upadates.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+
+        int height = lines.size();
+        int width = lines.get(0).length();
+
+        char[][] area = new char[height][width];
+        for (int i = 0; i < lines.size(); i++) {
+            area[i] = lines.get(i).toCharArray();
+        }
+
+        Set<Position> allAntennas = new HashSet<>();
+        Map<Character, List<Position>> antennas = new HashMap<>();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (area[y][x] == '.') {
+                    continue;
+                }
+
+                antennas.computeIfAbsent(area[y][x], k -> new ArrayList<>()).add(new Position(x, y));
+                allAntennas.add(new Position(x, y));
+            }
+        }
+
+        Set<Position> positions = new HashSet<>();
+        for (Map.Entry<Character, List<Position>> entry : antennas.entrySet()) {
+            for (Position first : entry.getValue()) {
+                for (Position second : entry.getValue()) {
+                    if (first.equals(second)) {
+                        continue;
+                    }
+
+                    int xDiv = second.x - first.x;
+                    int yDiv = second.y - first.y;
+
+                    int pX = first.x;
+                    int pY = first.y;
+                    while (inBound(height, width, pX - xDiv, pY - yDiv)) {
+                        positions.add(new Position(pX - xDiv, pY - yDiv));
+                        pX -= xDiv;
+                        pY -= yDiv;
+                    }
+
+                    pX = second.x;
+                    pY = second.y;
+                    while (inBound(height, width, pX + xDiv, pY + yDiv)) {
+                        positions.add(new Position(pX + xDiv, pY + yDiv));
+                        pX += xDiv;
+                        pY += yDiv;
+                    }
+                }
+            }
+        }
+
+        positions.addAll(allAntennas);
+
+        System.out.println(positions);
+
+        System.out.println(positions.size());
+    }
+
+    private static boolean inBound(int height, int width, int x, int y) {
+        if (y < 0) {
+            return false;
+        }
+
+        if (x < 0) {
+            return false;
+        }
+
+        if (x >= width) {
+            return false;
+        }
+
+        if (y >= height) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private record Position(int x, int y) {
+
+    }
+}
