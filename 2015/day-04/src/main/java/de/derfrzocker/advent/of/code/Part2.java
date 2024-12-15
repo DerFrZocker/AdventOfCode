b18ad59ab082b64857c532baf3e8d8eacf621f9b
DerFrZocker <git@derfrzocker.de>
2024-12-15 12:50:04 +0100

    Advent Of Code 2015 Day 04 Part 2

---
+++ 2015/day-04/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,39 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        String input = lines.get(0);
+
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+
+        long index = 0;
+        dance:
+        while (true) {
+            byte[] hash = digest.digest((input + index).getBytes());
+
+            for (int i = 0; i < 3; i++) {
+                if (hash[i] != 0) {
+                    index++;
+                    continue dance;
+                }
+            }
+
+            break;
+        }
+
+        System.out.println(index);
+    }
+}
