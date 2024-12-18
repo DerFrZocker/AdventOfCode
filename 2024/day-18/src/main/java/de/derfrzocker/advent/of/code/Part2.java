67351e3b8108ce80513aff04bf652e817908ab40
DerFrZocker <git@derfrzocker.de>
2024-12-18 21:15:14 +0100

    Advent Of Code 2024 Day 18 Part 2

---
+++ 2024/day-18/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,114 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    static int height = 70 + 1;
+    static int width = 70 + 1;
+    static int[][] maze = new int[height][width];
+
+    static int startX = 0;
+    static int startY = 0;
+
+    static int endX = width - 1;
+    static int endY = height - 1;
+
+    private static final int WALL = -1;
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                maze[y][x] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            String[] parts = lines.get(i).split(",");
+            int x = Integer.parseInt(parts[0]);
+            int y = Integer.parseInt(parts[1]);
+
+            maze[y][x] = WALL;
+
+            if (!solvable()) {
+                System.out.println("Not solvable " + i);
+                System.out.println("%d,%d".formatted(x, y));
+                return;
+            }
+        }
+    }
+
+    private static boolean solvable() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (maze[y][x] != WALL) {
+                    maze[y][x] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        maze[startY][startX] = 0;
+
+        List<Position> toCheck = new ArrayList<>();
+        toCheck.add(new Position(startX, startY));
+        while (!toCheck.isEmpty()) {
+            Position checking = toCheck.removeFirst();
+
+            for (Direction direction : Direction.values()) {
+                int newX = checking.x() + direction.getXOffset();
+                int newY = checking.y() + direction.getYOffset();
+
+                if (!inBound(newX, newY) || maze[newY][newX] == WALL) {
+                    continue;
+                }
+
+                int cost = maze[checking.y()][checking.x()] + 1;
+
+                if (cost < maze[newY][newX]) {
+                    maze[newY][newX] = cost;
+                    toCheck.add(new Position(newX, newY));
+                }
+
+                if (newX == endX && newY == endY) {
+                    System.out.println(cost);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean inBound(Position postion) {
+        return inBound(postion.x(), postion.y());
+    }
+
+    private static boolean inBound(int x, int y) {
+        if (y < 0) {
+            return false;
+        }
+
+        if (x < 0) {
+            return false;
+        }
+
+        if (x >= width) {
+            return false;
+        }
+
+        if (y >= height) {
+            return false;
+        }
+
+        return true;
+    }
+}
