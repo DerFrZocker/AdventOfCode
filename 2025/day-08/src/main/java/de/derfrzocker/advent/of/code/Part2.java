e4858d5091eed92aa582995225cc74d532429601
DerFrZocker <git@derfrzocker.de>
2025-12-08 19:57:43 +0100

    Advent Of Code 2025 Day 08 Part 2

---
+++ 2025/day-08/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,109 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    private static List<Set<Position>> circ = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        List<Position> positions = new ArrayList<>();
+        for (String line : lines) {
+            String[] split = line.split(",");
+            positions.add(new Position(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+        }
+
+        for (Position position : positions) {
+            Set<Position> set = new HashSet<>();
+            set.add(position);
+            circ.add(set);
+        }
+
+        Map<Pair, Long> distances = new HashMap<>();
+        for (Position first : positions) {
+            for (Position second : positions) {
+                if (first.equals(second)) {
+                    continue;
+                }
+                if (distances.containsKey(new Pair(first, second))) {
+                    continue;
+                }
+                if (distances.containsKey(new Pair(second, first))) {
+                    continue;
+                }
+
+                distances.put(new Pair(first, second), calc(first, second));
+            }
+        }
+
+        List<Map.Entry<Pair, Long>> entries = new ArrayList<>(distances.entrySet());
+        entries.sort(Comparator.comparingLong(Map.Entry::getValue));
+
+        for (Map.Entry<Pair, Long> entry : entries) {
+            Position first = entry.getKey().first();
+            Position second = entry.getKey().second();
+            if (!isInSameCirc(first, second)) {
+                addInCirc(first, second);
+            }
+
+            if (circ.size() == 1) {
+                System.out.println(first.x * second.x);
+                break;
+            }
+        }
+    }
+
+    private static boolean isInSameCirc(Position first, Position second) {
+        for (Set<Position> cirs : circ) {
+            if (cirs.contains(first) && cirs.contains(second)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void addInCirc(Position first, Position second) {
+        Set<Position> firstCirc = null;
+        Set<Position> secondCirc = null;
+        for (Set<Position> cirs : circ) {
+            if (cirs.contains(first)) {
+                firstCirc = cirs;
+            }
+            if (cirs.contains(second)) {
+                secondCirc = cirs;
+            }
+            if (firstCirc != null && secondCirc != null) {
+                break;
+            }
+        }
+
+        firstCirc.addAll(secondCirc);
+        circ.remove(secondCirc);
+    }
+
+    private static long calc(Position first, Position second) {
+        return (long) (Math.pow(first.x - second.x, 2) + Math.pow(first.y - second.y, 2) + Math.pow(first.z - second.z, 2));
+    }
+
+    private record Pair(Position first, Position second) {
+    }
+
+    public record Position(int x, int y, int z) {
+
+    }
+}
