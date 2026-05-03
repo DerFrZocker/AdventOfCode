9fe6116d102449e68d52b2ea8e5d5e61addd0514
DerFrZocker <git@derfrzocker.de>
2026-05-03 14:52:59 +0200

    Advent Of Code 2016 Day 06 Part 2

---
+++ 2016/day-06/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,55 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        List<List<Character>> strings = new ArrayList<>();
+
+        for (int i = 0; i < lines.getFirst().length(); i++) {
+            strings.add(new ArrayList<>());
+        }
+
+        for (String line : lines) {
+            for (int i = 0; i < line.length(); i++) {
+                List<Character> list = strings.get(i);
+
+                list.add(line.charAt(i));
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (List<Character> list : strings) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (Character character : list) {
+                map.compute(character, (c, count) -> count == null ? 1 : count + 1);
+            }
+
+            Character best = '$';
+            int amount = Integer.MAX_VALUE;
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                if (entry.getValue() < amount) {
+                    best = entry.getKey();
+                    amount = entry.getValue();
+                }
+            }
+
+            builder.append(best);
+        }
+
+        System.out.println(builder);
+    }
+}
