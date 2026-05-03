cb83993ddb73e251b1de5349aef3dea8e29d6ae0
DerFrZocker <git@derfrzocker.de>
2026-05-03 10:02:59 +0200

    Advent Of Code 2015 Day 25 Part 2

---
+++ 2015/day-25/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,18 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+
+    }
+}
