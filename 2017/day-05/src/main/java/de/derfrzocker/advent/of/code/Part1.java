53ab3e444e7e7e645cbee3bf3c130046d0c31ec4
DerFrZocker <git@derfrzocker.de>
2026-05-15 12:48:46 +0200

    Advent Of Code 2017 Day 05 Part 1

---
+++ 2017/day-05/src/main/java/de/derfrzocker/advent/of/code/Part1.java
@@ -0,0 +1,33 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part1 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        List<Integer> values = new ArrayList<>();
+        for (String line : lines) {
+            values.add(Integer.parseInt(line));
+        }
+
+        int counter = 0;
+        int step = 0;
+        while (counter < values.size()) {
+            step++;
+            int current = counter;
+            counter += values.get(current);
+            values.set(current, values.get(current) + 1);
+        }
+
+        System.out.println(step);
+    }
+}
