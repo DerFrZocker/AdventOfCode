957b634681035b06873c628231dd9ebad7283d6a
DerFrZocker <git@derfrzocker.de>
2026-05-18 20:12:19 +0200

    Advent Of Code 2017 Day 13 Part 2

---
+++ 2017/day-13/src/main/java/de/derfrzocker/advent/of/code/Part2.java
@@ -0,0 +1,48 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class Part2 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        NavigableMap<Integer, Integer> map = new TreeMap<>();
+        for (String line : lines) {
+            String[] split = line.split(": ");
+            int depth = Integer.parseInt(split[0]);
+            int range = Integer.parseInt(split[1]);
+
+            map.put(depth, range);
+        }
+
+        int result = -1;
+        dance: while (true) {
+            result++;
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                int depth = entry.getKey();
+                int range = entry.getValue();
+
+                int position = (result + depth) % ((range * 2) - 2);
+
+                if (position == 0) {
+                    continue dance;
+                }
+            }
+
+            break;
+        }
+
+        System.out.println(result);
+    }
+}
