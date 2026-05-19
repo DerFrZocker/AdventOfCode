b952aaca413f01a5fea4f37d4823bcf76dbe9c7a
DerFrZocker <git@derfrzocker.de>
2026-05-19 21:21:23 +0200

    Advent Of Code 2017 Day 14 Part 1

---
+++ 2017/day-14/src/main/java/de/derfrzocker/advent/of/code/Part1.java
@@ -0,0 +1,119 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part1 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        String prefix = lines.getFirst();
+
+        int result = 0;
+        for (int i = 0; i < 128; i++) {
+            String hash = hash(prefix + "-" + i);
+            for (char c : hash.toCharArray()) {
+                if (c == '1') {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static String hash(String input) {
+        List<Integer> lengths = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            lengths.add((int) c);
+        }
+
+        lengths.add(17);
+        lengths.add(31);
+        lengths.add(73);
+        lengths.add(47);
+        lengths.add(23);
+
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+            integers.add(i);
+        }
+
+        int skip = 0;
+        int index = 0;
+        for (int u = 0; u < 64; u++) {
+            for (int length : lengths) {
+                for (int i = 0; i < (length / 2); i++) {
+                    int first = (index + i) % integers.size();
+                    int second = (index + (length - 1) - i) % integers.size();
+                    int tmp = integers.get(first);
+                    integers.set(first, integers.get(second));
+                    integers.set(second, tmp);
+                }
+
+                index += length;
+                index += skip;
+                index = index % integers.size();
+                skip++;
+            }
+        }
+
+        List<Integer> denseHash = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            int value = 0;
+            for (int u = 0; u < 16; u++) {
+                value = value ^ integers.get((i * 16) + u);
+            }
+
+            denseHash.add(value);
+        }
+
+        return denseHash.stream().map(Integer::toBinaryString).map(Part1::pad).reduce(String::concat).get();
+    }
+
+    private static String pad(String value) {
+        if (value.length() == 8) {
+            return value;
+        }
+        if (value.isEmpty()) {
+            return "00000000";
+        }
+
+        if (value.length() == 1) {
+            return "0000000" + value;
+        }
+
+        if (value.length() == 2) {
+            return "000000" + value;
+        }
+
+        if (value.length() == 3) {
+            return "00000" + value;
+        }
+
+        if (value.length() == 4) {
+            return "0000" + value;
+        }
+
+        if (value.length() == 5) {
+            return "000" + value;
+        }
+
+        if (value.length() == 6) {
+            return "00" + value;
+        }
+
+        if (value.length() == 7) {
+            return "0" + value;
+        }
+
+        throw new RuntimeException();
+    }
+}
