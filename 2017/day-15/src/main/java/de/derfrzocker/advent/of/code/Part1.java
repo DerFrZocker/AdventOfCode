d13c0cda2b8c7de66d9d6809017458b34cf5468f
DerFrZocker <git@derfrzocker.de>
2026-05-20 18:45:36 +0200

    Advent Of Code 2017 Day 15 Part 1

---
+++ 2017/day-15/src/main/java/de/derfrzocker/advent/of/code/Part1.java
@@ -0,0 +1,47 @@
+package de.derfrzocker.advent.of.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Part1 {
+
+    private static final File DATA = new File("data.txt");
+    private static final File EXAMPLE = new File("example.txt");
+
+    private static final int ITERATIONS = 40_000_000;
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(DATA.toPath());
+
+        long a = 0;
+        long b = 0;
+
+        for (String line : lines) {
+            if (line.startsWith("Generator A starts with")) {
+                a = Long.parseLong(line.substring(line.lastIndexOf(" ") + 1));
+            } else if (line.startsWith("Generator B starts with")) {
+                b = Long.parseLong(line.substring(line.lastIndexOf(" ") + 1));
+            } else {
+                throw new IllegalArgumentException("Invalid line " + line);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < ITERATIONS; i++) {
+            a = next(a, 16807);
+            b = next(b, 48271);
+
+            if ((a & 0b1111111111111111) == (b & 0b1111111111111111)) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static long next(long prev, long multi) {
+        return Math.multiplyExact(prev, multi) % 2147483647;
+    }
+}
